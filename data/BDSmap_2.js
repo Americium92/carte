085d5865595256a733f79fe74687c20dd93aa177
Americium92 <56517067+Americium92@users.noreply.github.com>
2020-09-28 23:16:14 +0200

    Update BDSmap_2.js

--- data/BDSmap_2.js
+++ data/BDSmap_2.js
@@ -10,7 +10,7 @@ var json_BDSmap_2 = {
 { "type": "Feature", "properties": { "Sport": "Handball", "Crénaux 1": "lundi 18h-20h", "Crénaux 2": "mercredi 18h-20h", "x": 2.212628, "y": 48.711458, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.212628, 48.711458 ] } },
 { "type": "Feature", "properties": { "Sport": "Aviron", "Crénaux 1": "jeudi 13h-17h30", "Crénaux 2": null, "x": null, "y": null, "field_6": null, "field_7": null }, "geometry": null },
 { "type": "Feature", "properties": { "Sport": "Beach Volley", "Crénaux 1": "lundi 20h-20h", "Crénaux 2": null, "x": 2.19412, "y": 48.711086, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.19412, 48.711086 ] } },
-{ "type": "Feature", "properties": { "Sport": "Bolley ", "Crénaux 1": "lundi 20h-20h", "Crénaux 2": null, "x": 2.213003, "y": 48.711469, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.213003, 48.711469 ] } },
+{ "type": "Feature", "properties": { "Sport": "Volley ", "Crénaux 1": "lundi 20h-20h", "Crénaux 2": null, "x": 2.213003, "y": 48.711469, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.213003, 48.711469 ] } },
 { "type": "Feature", "properties": { "Sport": "Boxe", "Crénaux 1": "mercredi 19h 21h", "Crénaux 2": null, "x": 2.200908, "y": 48.712488, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.200908, 48.712488 ] } },
 { "type": "Feature", "properties": { "Sport": "Pompom", "Crénaux 1": "lundi 18h30-21h ", "Crénaux 2": "mardi 18h30-21h", "x": 2.201313, "y": 48.712373, "field_6": null, "field_7": null }, "geometry": { "type": "Point", "coordinates": [ 2.201313, 48.712373 ] } }
 ]
